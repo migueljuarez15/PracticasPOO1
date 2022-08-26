cef204b64b6ce7a16253ddc1077fb0d28c6cb791
Miguel Juárez <emekaerre15jk@gmail.com>
2022-08-26 10:56:40 -0500

    Practica02 9:20

---
+++ Practica02/PruebaSaludador.java
@@ -0,0 +1,9 @@
+// Método principal (main).
+public class PruebaSaludador{
+	public static void main(String[] args){
+		// Creando la instancia del Objeto Saludador.
+		Saludador alumno = new Saludador();
+		// Accediendo al método saludar del alumno.
+		alumno.saludar();
+	}
+}
---
+++ Practica02/Saludador.java
@@ -0,0 +1,7 @@
+// Definición de la clase.
+public class Saludador{
+	// Método para saludar.
+	public void saludar(){ // Llave que inicia el main.
+		System.out.println("Hola Alumno de POO");
+	}// Llave que cierra el main de saludar.
+}// Llave que cierra la clase.
