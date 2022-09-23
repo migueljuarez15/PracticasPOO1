b3e364d062614c4d6c47f1347bb4cdaa27f05dc8
Miguel Juárez <emekaerre15jk@gmail.com>
2022-09-23 09:08:28 -0500

    Practica07 9:08
    Paso 9

--- Practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
+++ Practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -21,30 +21,30 @@ public class PruebaCiclos{
 		for (int i = n; i < m; i++) {
 			int ed = ((int)(Math.random()*250));
 			if (per01.setEdad(ed)){
-				System.out.println(per01.getDetalle() + "se modifico la edad");
+				System.out.println(per01.getDetalle() + " se modifico la edad");
 			}else{
-				System.out.println(per01.getDetalle() + "sin modificacion en edad a " + ed);
+				System.out.println(per01.getDetalle() + " sin modificacion en edad a " + ed);
 			} // CIERRE IF per01
 			ed = ((int)(Math.random()*250));
 			if (per02.setEdad(ed)){
-				System.out.println(per02.getDetalle() + "se modifico la edad");
+				System.out.println(per02.getDetalle() + " se modifico la edad");
 			}else{
-				System.out.println(per02.getDetalle() + "sin modificacion en edad a " + ed);
+				System.out.println(per02.getDetalle() + " sin modificacion en edad a " + ed);
 			} // CIERRE IF per02
 			ed = ((int)(Math.random()*250));
 			if (per03.setEdad(ed)){
-				System.out.println(per03.getDetalle() + "se modifico la edad");
+				System.out.println(per03.getDetalle() + " se modifico la edad");
 			}else{
-				System.out.println(per03.getDetalle() + "sin modificacion en edad a " + ed);
+				System.out.println(per03.getDetalle() + " sin modificacion en edad a " + ed);
 			} // CIERRE IF per03
 			do{
 				ed = ((int)(Math.random()*200));
 				System.out.println(ed);
 				if (ed > 150){
 					System.out.println("*** El numero generado es mayor a 150 ***");
+					continue outfor;
 				} // CIERRE IF per04
 			}while (!per04.setEdad(ed));
-			break outfor;
 		} // CIERRE FOR
 		System.out.println(per04.getDetalle());
 	} // CIERRE MAIN
