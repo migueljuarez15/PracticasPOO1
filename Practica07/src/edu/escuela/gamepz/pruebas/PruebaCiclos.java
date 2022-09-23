297b0791e6e2a89b09588abb21aeac905ebbf011
Miguel Juárez <emekaerre15jk@gmail.com>
2022-09-23 08:58:06 -0500

    Practica07 8:58
    Paso 8

--- Practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
+++ Practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -1,3 +1,6 @@
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.Personaje;
+
 public class PruebaCiclos{
 	public static void main(String[] args) {
 		Personaje per01 = new Personaje("Diego");
@@ -10,40 +13,39 @@ public class PruebaCiclos{
 		while (n >= m){
 			n = ((int)(Math.random()*100));
 			m = ((int)(Math.random()*100));
-		}
+		} // CIERRE WHILE
 		System.out.println("n = " + n);
 		System.out.println("m = " + m);
 
 		outfor:
-		for (i = n; i < m; i++) {
+		for (int i = n; i < m; i++) {
 			int ed = ((int)(Math.random()*250));
 			if (per01.setEdad(ed)){
-				System.out.println(per01.getDetalle() + "se modificó la edad");
+				System.out.println(per01.getDetalle() + "se modifico la edad");
 			}else{
-				System.out.println(per01.getDetalle() + "sin modificación en edad a " + ed);
-			}
+				System.out.println(per01.getDetalle() + "sin modificacion en edad a " + ed);
+			} // CIERRE IF per01
 			ed = ((int)(Math.random()*250));
 			if (per02.setEdad(ed)){
-				System.out.println(per02.getDetalle() + "se modificó la edad");
+				System.out.println(per02.getDetalle() + "se modifico la edad");
 			}else{
-				System.out.println(per02.getDetalle() + "sin modificación en edad a " + ed);
-			}
+				System.out.println(per02.getDetalle() + "sin modificacion en edad a " + ed);
+			} // CIERRE IF per02
 			ed = ((int)(Math.random()*250));
 			if (per03.setEdad(ed)){
-				System.out.println(per03.getDetalle() + "se modificó la edad");
+				System.out.println(per03.getDetalle() + "se modifico la edad");
 			}else{
-				System.out.println(per03.getDetalle() + "sin modificación en edad a " + ed);
-			}
+				System.out.println(per03.getDetalle() + "sin modificacion en edad a " + ed);
+			} // CIERRE IF per03
 			do{
 				ed = ((int)(Math.random()*200));
 				System.out.println(ed);
 				if (ed > 150){
-					System.out.println("*** El número generado es mayor a 150 ***");
-				}
-			}while (!per04.setEdad(ed)){
-				break outfor;
-			}
-			System.out.println(per04.getDetalle());
-		}
-	}
-}
+					System.out.println("*** El numero generado es mayor a 150 ***");
+				} // CIERRE IF per04
+			}while (!per04.setEdad(ed));
+			break outfor;
+		} // CIERRE FOR
+		System.out.println(per04.getDetalle());
+	} // CIERRE MAIN
+} // CIERRE CLASE
