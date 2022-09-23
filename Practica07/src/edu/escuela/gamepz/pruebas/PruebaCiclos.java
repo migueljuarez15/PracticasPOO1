f8823c2ee3f3175507ca7aeb2928db7bf8689b8b
Miguel Juárez <emekaerre15jk@gmail.com>
2022-09-23 08:12:20 -0500

    Practica07 8:12
    Paso 5

--- Practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
+++ Practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -17,6 +17,16 @@ public class PruebaCiclos{
 		for (i = n; i < m; i++) {
 			int ed = ((int)(Math.random()*250))
 			per01.setEdad(ed);
+			System.out.println(per01.getDetalle() + "se modificó la edad");
+			System.out.println(per01.getDetalle() + "sin modificación en edad a " + ed);
+			ed = ((int)(Math.random()*250))
+			per02.setEdad(ed);
+			System.out.println(per02.getDetalle() + "se modificó la edad");
+			System.out.println(per02.getDetalle() + "sin modificación en edad a " + ed);
+			ed = ((int)(Math.random()*250))
+			per03.setEdad(ed);
+			System.out.println(per03.getDetalle() + "se modificó la edad");
+			System.out.println(per03.getDetalle() + "sin modificación en edad a " + ed);
 		}
 	}
 }
