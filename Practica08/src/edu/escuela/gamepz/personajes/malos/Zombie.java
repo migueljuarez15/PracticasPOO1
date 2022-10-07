a73249571089497731b131ebf051272b026c0c6b
Miguel Juárez <emekaerre15jk@gmail.com>
2022-10-07 09:20:36 -0500

    Practica08 9:19
    Paso 8

--- Practica08/src/edu/escuela/gamepz/personajes/malos/Zombie.java
+++ Practica08/src/edu/escuela/gamepz/personajes/malos/Zombie.java
@@ -18,4 +18,27 @@ public class Zombie extends Personaje{
 	public boolean getAtaque(){
 		return ataque;
 	} //CIERRE getAtaque
+	public void decVida(){
+		super.decVida();
+	} //CIERRE decVida()
+	public void decVida(int vida){
+		super.decVida(vida);
+		if (ataque == false){
+			vida *= 3;
+		}else{
+			vida *= 2;
+		}
+	} //CIERRE decVida(int)
+	public void addVida(){
+		super.addVida();
+	} //CIERRE addVida()
+	public void addVida(int vida){
+		super.addVida(vida);
+		if (ataque = true){
+			vida *= 3;
+		}
+	} //CIERRE addVida(int)
+	public String getDetalle(){
+		return super.getDetalle() + "\t" + ataque;
+	} //CIERRE getDetalle
 } //CIERRE CLASE
---
+++ Practica08/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
@@ -0,0 +1,5 @@
+public class PruebaHerencia{
+	public static void main(String[] args) {
+		
+	}
+}
