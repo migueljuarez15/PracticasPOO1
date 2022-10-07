89264a5f188a2f0ad30d2b2bfb443150ca8ad363
Miguel Juárez <emekaerre15jk@gmail.com>
2022-10-07 10:30:15 -0500

    Practica08 10:30
    Paso 9

--- Practica08/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
+++ Practica08/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
@@ -1,5 +1,15 @@
 public class PruebaHerencia{
 	public static void main(String[] args) {
-		Personaje p = new Personaje();
+		Personaje per01 = new Personaje("David", 100);
+		Personaje per02 = new Personaje("Bianca");
+		Planta plan01 = new Planta("Fabian", 10, 'B');
+		Planta plan02 = new Planta("Almendra", 50);
+		Planta plan03 = new Planta("Ricardo", 'C');
+		Planta plan04 = new Planta("Silvia");
+		Zombie zom01 = new Zombie("Armando", 80, false);
+		Zombie zom02 = new Zombie("Josseline", true);
+		Zombie zom03 = new Zombie("Eduardo");
+
+		Personaje pers[] = {per01, per02, plan01, plan02, plan03, plan04, zom01, zom02, zom03}
 	}
 }
