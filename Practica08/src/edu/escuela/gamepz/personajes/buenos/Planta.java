e0f8a79a41798cb7578335186e2f64de752748a0
Miguel Juárez <emekaerre15jk@gmail.com>
2022-10-07 08:32:55 -0500

    Practica08 8:32
    Paso 5

--- Practica08/src/edu/escuela/gamepz/personajes/buenos/Planta.java
+++ Practica08/src/edu/escuela/gamepz/personajes/buenos/Planta.java
@@ -6,21 +6,23 @@ public class Planta extends Personaje{
 	public Planta(String nombre, int vida, char escudo){
 		super(nombre, vida);
 		this.escudo = escudo;
-	}
+	} //CIERRE Planta1(String, int, char)
 	public Planta(String nombre, char escudo){
 		super(nombre, 3);
 		this.escudo = escudo;
-	}
+	} //CIERRE Planta2(String, char)
 	public Planta(String nombre, int vida){
 		super(nombre, vida);
 		escudo = 'A';
-	}
+	} //CIERRE Planta3(String, int)
 	public Planta(String nombre){
 		super(nombre, 3);
 		escudo = 'A';
-	}
-
+	} //CIERRE Planta4(String)
 	public char getEscudo(){
 		return escudo;
-	}
+	} //CIERRE getEscudo
+	public String getDetalle(){
+		return super.getDetalle() + "\t" + escudo;
+	} //CIERRE getDetalle
 } //CIERRE CLASE
