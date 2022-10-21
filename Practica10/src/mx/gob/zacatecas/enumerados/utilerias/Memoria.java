1e9e091bf08dcd74a988c0583e2ef28d55638427
Miguel Juárez <emekaerre15jk@gmail.com>
2022-10-21 08:02:27 -0500

    Practica10 8:02
    Clase Enumerada

---
+++ Practica10/src/mx/gob/zacatecas/enumerados/utilerias/Memoria.java
@@ -0,0 +1,23 @@
+public enum Memoria{
+	DDR("Memoria RAM", 166, 30),
+	DDR2("Laptop", 240, 35),
+	DDR3("Laptop Nueva", 533, 40),
+	DDR4("Gamer", 1600, 40);
+	String tipo;
+	float vel;
+	int pines;
+	private Memoria(String tipo, float vel, int pines){
+		this.tipo = tipo;
+		this.vel = vel;
+		this.pines = pines;
+	}
+	public String getTipo(){
+		return tipo;
+	}
+	public float getVelocidad(){
+		return vel;
+	}
+	public int getPines(){
+		return pines;
+	}
+}
