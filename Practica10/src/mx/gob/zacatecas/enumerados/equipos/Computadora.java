4fe666389e462c07187a7fdfde41c8ba19491535
Miguel Juárez <emekaerre15jk@gmail.com>
2022-10-21 08:30:49 -0500

    Practica10 8:30
    Fallo en compilación

---
+++ Practica10/src/mx/gob/zacatecas/enumerados/equipos/Computadora.java
@@ -0,0 +1,18 @@
+package mx.gob.zacatecas.enumerados.equipos;
+import mx.gob.zacatecas.enumerados.utilerias.*;
+public class Computadora{
+	private String marca;
+	private Tipo tipo;
+	private Memoria memoria;
+	public Computadora(String marca, Tipo tipo, Memoria memoria){
+		this.marca = marca;
+		this.tipo = tipo;
+		this.memoria = memoria;
+	}
+	public String getMarca(){
+		return marca;
+	}
+	public String toString(){
+		return marca + " " + memoria.getTipo() + " " + memoria.getVelocidad() + " " + memoria.getPines() + " " + tipo.getType();
+	}
+}
