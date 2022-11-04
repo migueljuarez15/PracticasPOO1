3cd1493bde24d72f5b5f7dda1dac574ab6e2e881
Miguel Juárez <emekaerre15jk@gmail.com>
2022-11-04 09:30:21 -0600

    Practica12 9:30
    Paso 5 (con error en el paso 4)

--- Practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ Practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -1,5 +1,6 @@
 package edu.escuela.gamepz.utils;
 import edu.escuela.gamepz.personajes.Personaje;
+import edu.escuela.gamepz.utils.PersException;
 public class Tablero{
 	public static final int MAX_SIZE = 10;
 	public static Personaje personajes[] = new Personaje[MAX_SIZE];
@@ -16,5 +17,12 @@ public class Tablero{
 			}
 		}
 		System.out.println("\n Fin *** Contenido del arreglo en Tablero");
+	}
+	public void insertar(Personaje p, int poss) throws PersException{
+		if(poss < 0 | poss > MAX_SIZE){
+			throw new PersException("Indice fuera de rango ", poss)
+		}else{
+			p.getPoss();
+		}
 	}
 }
