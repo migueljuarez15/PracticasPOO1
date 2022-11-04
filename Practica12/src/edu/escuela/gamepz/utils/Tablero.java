bfae6910ae13b0506bf22108c3c9eb7436b459c7
Miguel Juárez <emekaerre15jk@gmail.com>
2022-11-04 09:15:50 -0600

    Practica 9:15
    Paso 4

--- Practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ Practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -2,12 +2,19 @@ package edu.escuela.gamepz.utils;
 import edu.escuela.gamepz.personajes.Personaje;
 public class Tablero{
 	public static final int MAX_SIZE = 10;
-	Personaje personajes[] = new Personaje[MAX_SIZE];
+	public static Personaje personajes[] = new Personaje[MAX_SIZE];
 	private Tablero(){}
-	public void mostrar(){
+	public static void mostrar(){
 		System.out.println("\n Inicio *** Contenido del arreglo en Tablero");
+		int cont = 0;
 		for (Personaje tmp: personajes){
-			
+			cont++;
+			if(tmp == null){
+				System.out.println(cont + " - - - ");
+			}else{
+				System.out.println(cont + personajes[]);
+			}
 		}
+		System.out.println("\n Fin *** Contenido del arreglo en Tablero");
 	}
 }
