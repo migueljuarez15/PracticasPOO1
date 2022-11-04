bfd84610d597a4b542b2b022872491f0e000c65e
Miguel Juárez <emekaerre15jk@gmail.com>
2022-11-04 10:16:03 -0600

    Practica12 10:15
    Paso 7 (con error en paso 6)

---
+++ Practica12/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
@@ -0,0 +1,9 @@
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.Personaje;
+import edu.escuela.gamepz.personajes.buenos.Planta;
+import edu.escuela.gamepz.personajes.malos.Zombie;
+import edu.escuela.gamepz.utils.*;
+
+public class PruebaTablero{
+	
+}
