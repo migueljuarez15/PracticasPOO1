680bfcf15bc2946e3b1ed7813a02d0dd41644f6a
Miguel Juárez <emekaerre15jk@gmail.com>
2022-11-11 09:45:39 -0600

    Practica13 9:45
    Paso 1

---
+++ Practica13/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
@@ -0,0 +1,39 @@
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.Personaje;
+import edu.escuela.gamepz.personajes.buenos.Planta;
+import edu.escuela.gamepz.personajes.malos.Zombie;
+import edu.escuela.gamepz.utils.*;
+
+public class PruebaTablero{
+	public static void main(String[] args) {
+		Personaje datos[] = {
+			new Planta("David", 100), 
+			new Zombie("Bianca"), 
+			new Planta("Fabian", 10, Escudo.MEDIO), 
+			new Planta("Almendra", 50), 
+			new Planta("Ricardo", Escudo.BAJO), 
+			new Planta("Silvia"), 
+			new Zombie("Armando", 80, false), 
+			new Zombie("Josseline", true), 
+			new Zombie("Eduardo")
+		}; //CIERRE ARREGLOS
+		for (Personaje p: datos) {
+			int poss = (int)(Math.random()*20-5);
+			try{
+				Tablero.insertar(p, poss);
+			}catch(PersException pe){
+				System.err.println(pe.getMessage() + " " + pe.getPoss());
+			}
+		}
+		Tablero.mostrar();
+		for (int i = 0; i < Tablero.MAX_SIZE; i++) {
+			int poss = (int)(Math.random()*20-5);
+			try{
+				Tablero.borrar(poss);
+			}catch(PersException pe){
+				System.err.println(pe.getMessage() + " " + pe.getPoss());
+			}	
+		}
+		Tablero.mostrar();
+	}
+}
---
+++ Practica13/src/edu/escuela/gamepz/utils/Tablero.java
@@ -0,0 +1,36 @@
+package edu.escuela.gamepz.utils;
+import edu.escuela.gamepz.personajes.Personaje;
+import edu.escuela.gamepz.utils.PersException;
+public class Tablero{
+	public static final int MAX_SIZE = 10;
+	public static Personaje personajes[] = new Personaje[MAX_SIZE];
+	private Tablero(){}
+	public static void mostrar(){
+		System.out.println("\n Inicio *** Contenido del arreglo en Tablero");
+		int cont = -1;
+		for (Personaje tmp: personajes){
+			cont++;
+			if(tmp == null){
+				System.out.println(cont + " - - - ");
+			}else{
+				System.out.println(cont + " " + tmp);
+			}
+		}
+		System.out.println("\n Fin *** Contenido del arreglo en Tablero");
+	}
+	public static void insertar(Personaje per, int poss) throws PersException{
+		if(poss < 0 || poss >= MAX_SIZE){
+			throw new PersException("Indice fuera de rango ", poss);
+		}
+		personajes[poss] = per;
+	}
+	public static void borrar(int poss) throws PersException{
+		if(poss < 0 || poss >= MAX_SIZE){
+			throw new PersException("Indice fuera de rango ", poss);
+		}
+		if(personajes[poss] == null){
+			throw new PersException("Si personaje para borrar ", poss);
+		}
+		personajes[poss] = null;
+	}
+}
