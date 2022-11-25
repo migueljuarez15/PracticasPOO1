aaf270ed5a435517425e678c75a676fd899b179a
Miguel Juárez <emekaerre15jk@gmail.com>
2022-11-25 08:39:32 -0600

    Practica15 8:39
    Paso 6

---
+++ Practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -0,0 +1,4 @@
+package mx.com.hilos.corredores.hilos;
+public class RunHilos{
+	
+}
--- Practica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
+++ Practica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
@@ -1,21 +1,26 @@
 package mx.com.hilos.corredores.ventanas;
+import mx.com.hilos.corredores.hilos.RunHilos;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.BorderFactory;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.FlowLayout;
 
 public class Ventana extends JFrame{
 	public Ventana(){
 		initComponents();
 	}
-	public void initComponents(){
+	private void initComponents(){
 		int ancho = 550;
 		int alto = 250;
 		Dimension lblDim = new Dimension(ancho - 15, alto / 10);
 		Color border = new Color(0, 0, 255);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		lblTitulo = new JLabel("* Practica 15 - Manejo de hilos *");
 		lblTitulo.setPreferredSize(lblDim);
 		lblElem1 = new JLabel("Hilo 1");
@@ -23,6 +28,12 @@ public class Ventana extends JFrame{
 		lblElem2 = new JLabel("Hilo 2");
 		txtElem2 = new JTextField(3);
 		btnStart = new JButton("Comenzar");
+		btnStart.addActionListener(new ActionListener(){
+			public void accionBtn(ActionEvent click){
+				btnStart.setEnable(true);
+				//RunHilos.iniciar();
+			}
+		});
 		hElem1 = new JLabel("* Aqui va el hilo 1 *");
 		hElem1.setBorder(BorderFactory.createLineBorder(border));
 		hElem1.setPreferredSize(lblDim);
@@ -30,6 +41,17 @@ public class Ventana extends JFrame{
 		hElem2.setBorder(BorderFactory.createLineBorder(border));
 		hElem2.setPreferredSize(lblDim);
 		btnStart.setPreferredSize(lblDim);
+		setTitle("Practica 15");
+		setLayout(new FlowLayout());
+		setSize(ancho, alto);
+		add(lblTitulo);
+		add(lblElem1);
+		add(txtElem1);
+		add(lblElem2);
+		add(txtElem2);
+		add(btnStart);
+		add(hElem1);
+		add(hElem2);
 	}
 	private JLabel lblTitulo;
 	private JLabel lblElem1;
